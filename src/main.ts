1fac4672108e11c552670ffd68ba4fa06c707da9
argon1025 <argon1025@gmail.com>
2022-04-10 00:00:14 +0900

    feat: swaggerDocumentBuilder main 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@api/app.module';
+import { swaggerDocumentBuilder } from '@app/library/open-api/';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  swaggerDocumentBuilder(app);
   await app.listen(3000);
 }
 bootstrap();
