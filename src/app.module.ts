337ef5ab1b5a458e62d67eb1238da6ad406e52bd
argon1025 <argon1025@gmail.com>
2022-04-09 21:30:48 +0900

    feat: class-transformer 글로벌 인터셉터 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { AppService } from '@api/app.service';
 import { AppController } from '@api/app.controller';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor },
+  ],
 })
 export class AppModule {}
