cb3eff552fa141e711e06546570230aaae1449e8
argon1025 <argon1025@gmail.com>
2022-04-10 16:29:37 +0900

    refactor: TODO 주석 삭제

--- library/all-exceptions/all-exception.filter.ts
+++ library/all-exceptions/all-exception.filter.ts
@@ -30,8 +30,6 @@ export class AllExceptionsFilter implements ExceptionFilter {
     // NOTE: exception-message.interface를 상속받는 object인지 확인합니다
     const isCustomException = isExceptionMessageInterface(customExceptionData);
 
-    // TODO: 커스텀 메시지 데이터가 존재하지 않을 경우 null을 리턴할지 undefined를 리턴할지 결정이 필요합니다
-    // TODO: 정하고 나서는 모든 response 응답에 대해 통일해야합니다
     const responseBody = {
       statusCode: httpStatusCode,
       requestLocation: request.url,
