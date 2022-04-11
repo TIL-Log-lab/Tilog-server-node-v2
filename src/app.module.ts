c457eb2f2f0fbcf7b743c94f7ebeca9c5d8d27c9
argon1025 <argon1025@gmail.com>
2022-04-11 22:24:52 +0900

    feat: configService 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,22 @@ import {
   Module,
   ValidationPipe,
 } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppService } from '@api/app.service';
 import { AppController } from '@api/app.controller';
 import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { AllExceptionsFilter } from '@app/library/all-exceptions';
+import validationSchema from '@env/validationSchema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `environments/.${process.env.NODE_ENV}.env`,
+      validationSchema,
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
