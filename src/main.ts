ab3224cd747990ae3852d43b81145d2769c2910b
argon1025 <argon1025@gmail.com>
2022-04-11 22:25:30 +0900

    feat: main.ts에 configService 참조하는 포트 번호 반영

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@api/app.module';
 import { swaggerDocumentBuilder } from '@app/library/open-api/';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get<ConfigService>(ConfigService);
+  const SERVER_PORT = configService.get<number>('SERVER_PORT', 3000);
+
   swaggerDocumentBuilder(app);
-  await app.listen(3000);
+
+  await app.listen(SERVER_PORT);
 }
 bootstrap();
