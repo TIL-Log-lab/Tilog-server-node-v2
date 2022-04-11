96c95bba982118d0ef1f5c631f79a6408dfe3a2a
Daphne-Dev <daphned@naver.com>
2022-04-11 23:48:37 +0900

    chore: 불필요한 스키마 삭제

---
+++ prisma/migrations/20220411143849_delete_table/migration.sql
@@ -0,0 +1,39 @@
+/*
+  Warnings:
+
+  - You are about to drop the `pinnedRepositories` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `pinnedRepositoryCategories` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `postsTags` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `tags` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `userblogCustomization` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `pinnedRepositoryCategories` DROP FOREIGN KEY `FK_pinnedRepositoryCategories_categoryID_category_id`;
+
+-- DropForeignKey
+ALTER TABLE `pinnedRepositoryCategories` DROP FOREIGN KEY `FK_pinnedRepositoryCategories_pinnedRepositoriesID_pinnedReposit`;
+
+-- DropForeignKey
+ALTER TABLE `postsTags` DROP FOREIGN KEY `FK_postsTags_postsID_posts_id`;
+
+-- DropForeignKey
+ALTER TABLE `postsTags` DROP FOREIGN KEY `FK_postsTags_tagsID_tags_id`;
+
+-- DropForeignKey
+ALTER TABLE `userblogCustomization` DROP FOREIGN KEY `FK_userblogCustomization_usersID_users_id`;
+
+-- DropTable
+DROP TABLE `pinnedRepositories`;
+
+-- DropTable
+DROP TABLE `pinnedRepositoryCategories`;
+
+-- DropTable
+DROP TABLE `postsTags`;
+
+-- DropTable
+DROP TABLE `tags`;
+
+-- DropTable
+DROP TABLE `userblogCustomization`;
