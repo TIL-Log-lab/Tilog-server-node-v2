575fbdb17174b96f0875be30b99b8d394fc9a94a
argon1025 <argon1025@gmail.com>
2022-04-11 21:53:30 +0900

    feat: 개발용 exception log 추가

--- library/all-exceptions/all-exception.filter.ts
+++ library/all-exceptions/all-exception.filter.ts
@@ -4,6 +4,7 @@ import {
   ArgumentsHost,
   HttpException,
   HttpStatus,
+  Logger,
 } from '@nestjs/common';
 import { HttpAdapterHost } from '@nestjs/core';
 import { isExceptionMessageInterface } from '@app/library/all-exceptions';
@@ -29,6 +30,9 @@ export class AllExceptionsFilter implements ExceptionFilter {
       : null;
     // NOTE: exception-message.interface를 상속받는 object인지 확인합니다
     const isCustomException = isExceptionMessageInterface(customExceptionData);
+
+    // TODO: 개발 참고용, 배포시 제외
+    Logger.error(isHttpException ? exception.getResponse() : exception);
 
     const responseBody = {
       statusCode: httpStatusCode,
