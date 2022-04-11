0273b3ac8d094326b803155f90612bb16954e4a6
argon1025 <argon1025@gmail.com>
2022-04-11 22:25:54 +0900

    fix: joi 모듈 가져오기 구문 수정

--- environments/validationSchema.ts
+++ environments/validationSchema.ts
@@ -1,7 +1,7 @@
-import Joi from 'joi';
+import * as Joi from 'joi';
 
 export default Joi.object({
   NODE_ENV: Joi.string().valid('local', 'dev', 'prod').default('local'),
-  PORT: Joi.number().default(3000),
+  SERVER_PORT: Joi.number().default(3000),
   DATABASE_URL: Joi.string().required(),
 });
