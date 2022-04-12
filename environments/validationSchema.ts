547bffa3ca14b4eea4b774fa0a439916bf67de2d
argon1025 <argon1025@gmail.com>
2022-04-12 17:55:12 +0900

    feat: 인증에 필요한 환경변수 추가

--- environments/validationSchema.ts
+++ environments/validationSchema.ts
@@ -4,4 +4,7 @@ export default Joi.object({
   NODE_ENV: Joi.string().valid('local', 'dev', 'prod').default('local'),
   SERVER_PORT: Joi.number().default(3000),
   DATABASE_URL: Joi.string().required(),
+  OAUTH_GITHUB_CLIENT_ID: Joi.string().required(),
+  OAUTH_GITHUB_CLIENT_SECRET: Joi.string().required(),
+  OAUTH_GITHUB_CALLBACK_URL: Joi.string().required(),
 });
