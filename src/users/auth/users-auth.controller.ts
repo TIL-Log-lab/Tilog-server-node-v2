e19b93c9e108426f537dd9c857de27980f6b8db4
argon1025 <argon1025@gmail.com>
2022-04-14 16:33:04 +0900

    feat: 액세스 토큰 발급 엔드포인트 추가

--- src/users/auth/users-auth.controller.ts
+++ src/users/auth/users-auth.controller.ts
@@ -1,12 +1,29 @@
+import { hasNotRefreshToken } from '@api/users/auth/errors/users-auth.error';
 import { UsersAuthService } from '@api/users/auth/users-auth.service';
 import { UpSertUserAndGetIdResponse } from '@api/users/types/users.service.type';
+import { CookieService } from '@app/utils/cookie/cookie.service';
 import { GithubAuthGuard } from '@app/utils/guards/auth/github-auth.guard';
-import { Controller, Delete, Get, Headers, Ip, Req, Res } from '@nestjs/common';
-import { CookieOptions, Response } from 'express';
+import {
+  Controller,
+  Delete,
+  Get,
+  Headers,
+  Ip,
+  Req,
+  Res,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Response, Request } from 'express';
 
+// TODO: 데코레이터 병합 필요
 @Controller('auth')
 export class UsersAuthController {
-  constructor(private readonly usersAuthService: UsersAuthService) {}
+  constructor(
+    private readonly usersAuthService: UsersAuthService,
+    private readonly configService: ConfigService,
+    private readonly cookieService: CookieService,
+  ) {}
 
   @Get('github/login')
   @GithubAuthGuard()
@@ -16,23 +33,36 @@ export class UsersAuthController {
 
   @Get('github/callback')
   @GithubAuthGuard()
-  githubLoginCallback(
+  async githubLoginCallback(
     @Req() { user }: { user: UpSertUserAndGetIdResponse },
     @Ip() userIp: string,
     @Headers('user-agent') userAgent: string,
     @Res({ passthrough: true }) response: Response,
   ) {
-    const refreshToken = this.usersAuthService.createRefreshToken({
+    const refreshToken = await this.usersAuthService.createRefreshToken({
       id: user.id,
       userIp,
       userAgent,
     });
-    const refreshTokenCookieOption: CookieOptions = {
-      httpOnly: true,
-      secure: false,
-      sameSite: 'strict',
-    };
-    response.cookie('refreshToken', refreshToken, refreshTokenCookieOption);
-    return undefined;
+    response.cookie(
+      'refreshToken',
+      refreshToken,
+      this.cookieService.refreshCookieOptions(),
+    );
+    return null;
+  }
+
+  @Get('access-token')
+  getAccessTokenUsingRefreshToken(
+    @Req() request: Request,
+    @Headers('user-agent') userAgent: string,
+  ) {
+    if (!this.cookieService.isRefreshTokenCookie(request.cookies))
+      throw new UnauthorizedException(hasNotRefreshToken);
+    const token = request.cookies.refreshToken;
+    return this.usersAuthService.verifyRefreshTokenAndCreateAccessToken(
+      token,
+      userAgent,
+    );
   }
 }
