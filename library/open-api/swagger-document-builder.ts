f42fcf6489ca216a34fde5f68cdc11698a06189a
argon1025 <argon1025@gmail.com>
2022-04-15 15:29:23 +0900

    feat: Swagger accessToken Authorization 옵션 추가

--- library/open-api/swagger-document-builder.ts
+++ library/open-api/swagger-document-builder.ts
@@ -5,9 +5,22 @@ const config = new DocumentBuilder()
   .setTitle('Tilog-v2')
   .setDescription('Tilog-Backend')
   .setVersion('2.0')
+  .addBearerAuth(
+    {
+      type: 'http',
+      scheme: 'bearer',
+      name: 'JWT',
+      in: 'header',
+    },
+    'Authorization',
+  )
   .build();
 
 export const swaggerDocumentBuilder = (app: INestApplication): void => {
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 };
