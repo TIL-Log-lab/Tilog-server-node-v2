46e195bd28c2fecbb9497d43ad7f868313761e80
argon1025 <argon1025@gmail.com>
2022-04-15 17:02:38 +0900

    feat: JWT 가드, 오류 메시지 작성

---
+++ library/utils/guards/auth/jwt-access-token.guard.ts
@@ -0,0 +1,23 @@
+import {
+  applyDecorators,
+  Injectable,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth } from '@nestjs/swagger';
+
+import { decodeAccessTokenFail } from '@api/users/auth/errors/users-auth.error';
+
+@Injectable()
+class JwtAccessToken extends AuthGuard('jwtAccessToken') {
+  handleRequest<TUser>(err: Error, user: TUser) {
+    if (err) {
+      throw new UnauthorizedException(decodeAccessTokenFail);
+    }
+    // NOTE: 토큰이 존재하지 않는다면 validate를 스킵하고 Boolean 타입을 리턴합니다.
+    return user;
+  }
+}
+export const JwtAccessTokenGuard = () =>
+  applyDecorators(UseGuards(JwtAccessToken), ApiBearerAuth('accessToken'));
