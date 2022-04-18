107e8aed12e08bce83fc02bd24a5766f1e23257a
argon1025 <argon1025@gmail.com>
2022-04-18 23:14:01 +0900

    feat: getPostDetail 서비스

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,14 +1,20 @@
-import { Injectable } from '@nestjs/common';
-import { posts } from '@prisma/client';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { posts, postView } from '@prisma/client';
 
 import { PostsRepository } from '@api/posts/posts.repository';
 import { PrismaService } from '@app/library/prisma';
+import { PostsViewRepository } from '@api/posts/view/posts-view.repository';
 
 @Injectable()
 export class PostsService {
   constructor(
     private readonly prismaService: PrismaService,
     private readonly postsRepository: PostsRepository,
+    private readonly postsViewRepository: PostsViewRepository,
   ) {}
 
   async createPost({
@@ -38,5 +44,42 @@ export class PostsService {
       markdownContent,
       isPrivate,
     });
+  }
+
+  async getPostDetail(
+    postId: posts['id'],
+    userId: posts['usersID'] | null,
+    userIp: postView['userIP'],
+  ) {
+    try {
+      // NOTE: 조회수 카운트 및 히스토리 기록
+      await this.prismaService.$transaction(async (prisma) => {
+        const hasViewed =
+          await this.postsViewRepository.getOneByPostIdAndUserIp(
+            prisma,
+            postId,
+            userIp,
+          );
+        if (hasViewed) throw new Error('hasViewed');
+        await this.postsViewRepository.create(prisma, postId, userIp);
+        await this.postsRepository.addViewCountById(prisma, postId);
+      });
+    } catch (error) {
+      if (!(error instanceof Error)) throw new InternalServerErrorException();
+    }
+    // NOTE: 게시글 조회
+    const searchResult = await this.postsRepository.getDetailFindById(
+      this.prismaService,
+      postId,
+    );
+
+    if (!searchResult) throw new NotFoundException();
+
+    // NOTE: 비밀글일 경우 본인만 열람이 가능하다
+    if (searchResult.private === 1) {
+      if (searchResult.usersID !== userId) throw new NotFoundException();
+    }
+
+    return searchResult;
   }
 }
