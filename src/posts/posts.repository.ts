15f5a01082730c86df84ea5b3aed2aee89a5028b
argon1025 <argon1025@gmail.com>
2022-04-18 14:21:30 +0900

    feat: PostsRepository.create 게시글 생성 로직

---
+++ src/posts/posts.repository.ts
@@ -0,0 +1,40 @@
+import { now } from '@app/utils/';
+import { Injectable } from '@nestjs/common';
+import { posts, PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PostsRepository {
+  create({
+    prismaConnection,
+    userId,
+    categoryId,
+    title,
+    subTitle,
+    thumbnailUrl,
+    markdownContent,
+    isPrivate,
+  }: {
+    prismaConnection: PrismaClient;
+    userId: posts['usersID'];
+    categoryId: posts['categoryID'];
+    title: posts['title'];
+    subTitle: posts['subTitle'];
+    thumbnailUrl: posts['thumbNailURL'];
+    markdownContent: posts['markDownContent'];
+    isPrivate: boolean;
+  }) {
+    return prismaConnection.posts.create({
+      data: {
+        usersID: userId,
+        categoryID: categoryId,
+        title,
+        subTitle,
+        thumbNailURL: thumbnailUrl,
+        markDownContent: markdownContent,
+        createdAt: now(),
+        createdDay: now(), // NOTE: 인기글 조회를 위한 DATE 형식저장
+        private: isPrivate ? 1 : 0,
+      },
+    });
+  }
+}
