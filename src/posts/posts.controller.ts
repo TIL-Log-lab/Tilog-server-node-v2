280c86c3661d0b412fa2d7729e15f76a245b6aa3
argon1025 <argon1025@gmail.com>
2022-04-18 23:14:19 +0900

    feat: 엔드포인트 생성 및 모듈연결

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,12 +1,22 @@
-import { Body, Controller, UnauthorizedException } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Ip,
+  Query,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 import { PostsService } from '@api/posts/posts.service';
-import { CreatePost } from '@api/posts/posts.decorator';
+import { CreatePost, GetPostDetail } from '@api/posts/posts.decorator';
 import { JwtUserId } from '@app/utils/decorators/jwt-user-Id.decorator';
 
 import { CreatePostRequestBodyDto } from '@api/posts/dto/create-post.dto';
 import { decodeAccessTokenFail } from '@api/users/auth/errors/users-auth.error';
 import { TokenPayload } from '@app/utils/token/types/token.type';
+import {
+  GetPostDetailRequestQueryDto,
+  GetPostDetailResponseDto,
+} from '@api/posts/dto/get-post-detail.dto';
 
 @Controller('posts')
 export class PostsController {
@@ -20,5 +30,38 @@ export class PostsController {
     if (!userId) throw new UnauthorizedException(decodeAccessTokenFail);
     await this.postsService.createPost({ userId, ...createPostRequestBodyDto });
     return null;
+  }
+
+  @GetPostDetail()
+  async getPostDetail(
+    @Query() { postId }: GetPostDetailRequestQueryDto,
+    @JwtUserId() { userId }: TokenPayload,
+    @Ip() userIp: string,
+  ) {
+    const result = await this.postsService.getPostDetail(
+      postId,
+      userId,
+      userIp,
+    );
+    return new GetPostDetailResponseDto({
+      id: result.id,
+      title: result.title,
+      subTitle: result.subTitle,
+      thumbnailUrl: result.thumbNailURL,
+      view: result.viewCounts,
+      like: result.likes,
+      content: result.markDownContent,
+      createdAt: result.createdAt,
+      updatedAt: result.updatedAt,
+      user: {
+        userId: result.users.id,
+        username: result.users.userName,
+        avatar: result.users.proFileImageURL,
+      },
+      category: {
+        categoryId: result.category.id,
+        name: result.category.categoryName,
+      },
+    });
   }
 }
