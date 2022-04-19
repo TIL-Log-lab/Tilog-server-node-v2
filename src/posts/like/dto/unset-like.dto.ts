dca1e8c69d297d92981a777a0612abe0699dd5a1
argon1025 <argon1025@gmail.com>
2022-04-19 16:40:45 +0900

    feat: postsLike DTO, decorator

---
+++ src/posts/like/dto/unset-like.dto.ts
@@ -0,0 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { postLike } from '@prisma/client';
+import { Transform } from 'class-transformer';
+import { IsNotEmpty } from 'class-validator';
+
+export class UnsetLikedRequestDto {
+  @IsNotEmpty()
+  @Transform(({ value }) => BigInt(value))
+  @ApiProperty({ type: 'string' })
+  postId: postLike['postsID'];
+}
---
+++ src/posts/like/posts-like.decorator.ts
@@ -0,0 +1,36 @@
+import { applyDecorators, Delete, Get, Post } from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
+
+import { HasLikedResponseDto } from '@api/posts/like/dto/has-liked.dto';
+
+import { JwtAccessTokenGuard } from '@app/utils/guards/auth/jwt-access-token.guard';
+
+export const HasLiked = () =>
+  applyDecorators(
+    Get(),
+    JwtAccessTokenGuard(),
+    ApiOkResponse({
+      description: '포스트 좋아요 유무 반환 성공',
+      type: HasLikedResponseDto,
+    }),
+  );
+
+export const SetLike = () =>
+  applyDecorators(
+    Post(),
+    JwtAccessTokenGuard(),
+    ApiOkResponse({
+      description: '포스트 좋아요 설정 성공',
+      type: undefined,
+    }),
+  );
+
+export const UnsetLike = () =>
+  applyDecorators(
+    Delete(),
+    JwtAccessTokenGuard(),
+    ApiOkResponse({
+      description: '포스트 좋아요 해제 성공',
+      type: undefined,
+    }),
+  );
