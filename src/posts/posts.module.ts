58442865c946f3c4edddee3122fd40b948b3e203
argon1025 <argon1025@gmail.com>
2022-04-19 16:42:05 +0900

    feat: postslike 로직 모듈 연결

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,12 +3,21 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '@app/library/prisma';
 import { PostsRepository } from '@api/posts/posts.repository';
 import { PostsViewRepository } from '@api/posts/view/posts-view.repository';
-import { PostsService } from './posts.service';
+import { PostsLikeController } from '@api/posts/like/posts-like.controller';
+import { PostsLikeService } from '@api/posts/like/posts-like.service';
+import { PostsLikeRepository } from '@api/posts/like/posts-like.repository';
 import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [PostsService, PostsRepository, PostsViewRepository],
-  controllers: [PostsController],
+  providers: [
+    PostsService,
+    PostsRepository,
+    PostsViewRepository,
+    PostsLikeService,
+    PostsLikeRepository,
+  ],
+  controllers: [PostsController, PostsLikeController],
 })
 export class PostsModule {}
