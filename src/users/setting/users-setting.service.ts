34db379b6ea46b3032a6e444f8e21beaf1051de4
argon1025 <argon1025@gmail.com>
2022-04-20 23:07:06 +0900

    feat: userProfile Service

--- src/users/setting/users-setting.service.ts
+++ src/users/setting/users-setting.service.ts
@@ -1,14 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { usersSetting } from '@prisma/client';
 
 import { PrismaService } from '@app/library/prisma';
 import { UsersSettingRepository } from '@api/users/setting/users-setting.repository';
+import { UsersRepository } from '@api/users/users.repository';
 
 @Injectable()
 export class UsersSettingService {
   constructor(
     private readonly prismaService: PrismaService,
     private readonly usersSettingRepository: UsersSettingRepository,
+    private readonly usersRepository: UsersRepository,
   ) {}
 
   async upsertSetting({
@@ -33,6 +35,30 @@ export class UsersSettingService {
       userId,
       settingType,
       content,
+    });
+  }
+
+  getUserProfileAndSetting(userId: usersSetting['userId']) {
+    return this.prismaService.$transaction(async (prisma) => {
+      const userSetting = await this.usersSettingRepository.getManyByUserId({
+        prismaConnection: prisma,
+        userId,
+      });
+
+      const userProfile = await this.usersRepository.findOneById({
+        prismaConnection: prisma,
+        userId,
+      });
+      if (!userProfile) throw new InternalServerErrorException();
+
+      return {
+        name: userProfile.userName,
+        avatar: userProfile.proFileImageURL,
+        createdAt: userProfile.createdAt,
+        settings: userSetting.map((val) => {
+          return { type: val.type, content: val.content };
+        }),
+      };
     });
   }
 }
