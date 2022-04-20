34ffbc4db6a91db99db7da01099e3e368c5b49f2
argon1025 <argon1025@gmail.com>
2022-04-20 15:22:39 +0900

    feat: posts.controller, decorator, DTO

---
+++ src/posts/dto/get-posts-detail.dto.ts
@@ -0,0 +1,72 @@
+import { PostSearchDateScope, PostSearchSortScope } from '@app/utils/';
+import { ApiProperty } from '@nestjs/swagger';
+import { category, posts, users } from '@prisma/client';
+import { Transform, Type } from 'class-transformer';
+import { IsEnum, IsInt, IsNumber, IsOptional } from 'class-validator';
+import { number } from 'joi';
+
+export class GetPostsRequestQueryDto {
+  @IsEnum(PostSearchDateScope)
+  dateScope: PostSearchDateScope;
+
+  @IsEnum(PostSearchSortScope)
+  sortScope: PostSearchSortScope;
+
+  @IsInt()
+  @IsOptional()
+  @Type(() => Number)
+  userId?: posts['usersID'];
+
+  @IsInt()
+  @IsOptional()
+  @Type(() => Number)
+  categoryId?: posts['categoryID'];
+}
+
+class GetPostsItem {
+  @Transform(({ value }) => String(value))
+  @ApiProperty({ type: 'string' })
+  id: posts['id'];
+
+  title: posts['title'];
+
+  @ApiProperty({ type: String, nullable: true })
+  subTitle: posts['subTitle'];
+
+  @ApiProperty({ type: String, nullable: true })
+  thumbnailUrl: posts['thumbNailURL'];
+
+  view: posts['viewCounts'];
+
+  like: posts['likes'];
+
+  createdAt: posts['createdAt'];
+
+  user: GetPostsUserItem;
+
+  category: GetPostsCategoryItem;
+}
+
+class GetPostsUserItem {
+  userId: users['id'];
+
+  username: users['userName'];
+
+  @ApiProperty({ type: String, nullable: true })
+  avatar: users['proFileImageURL'];
+}
+
+class GetPostsCategoryItem {
+  categoryId: category['id'];
+
+  name: category['categoryName'];
+}
+
+export class GetPostsResponseDto {
+  @Type(() => GetPostsItem)
+  list: GetPostsItem[];
+
+  constructor(required: Required<GetPostsResponseDto>) {
+    Object.assign(this, required);
+  }
+}
