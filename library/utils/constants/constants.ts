fa28db9be4899a9fe2bf1cf68b4680a02125ad6f
argon1025 <argon1025@gmail.com>
2022-04-20 15:21:32 +0900

    feat: 검색 스코프 상수

--- library/utils/constants/constants.ts
+++ library/utils/constants/constants.ts
@@ -2,3 +2,16 @@ export const COUNTRY = {
   ko: 'ko',
   en: 'en',
 } as const;
+
+export enum PostSearchDateScope {
+  Daily = 1,
+  Weekly = 7,
+  Monthly = 30,
+  Yearly = 365,
+}
+
+export enum PostSearchSortScope {
+  like = 'likes',
+  view = 'viewCounts',
+  recently = 'createdAt',
+}
