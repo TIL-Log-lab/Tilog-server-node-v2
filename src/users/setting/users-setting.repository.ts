5960cf01dfc04f46e0c85fc7f5d8406a0d2c214b
argon1025 <argon1025@gmail.com>
2022-04-20 20:26:23 +0900

    feat: usersSetting.repository

---
+++ src/users/setting/users-setting.repository.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { OauthProvider, users, usersSetting } from '@prisma/client';
+
+import { now } from '@app/utils/';
+
+import { PrismaConnection } from '@app/library/prisma/type/prisma.type';
+
+@Injectable()
+export class UsersSettingRepository {
+  findOneByUserIdAndSettingType({
+    prismaConnection,
+    userId,
+    settingType,
+  }: {
+    prismaConnection: PrismaConnection;
+    userId: usersSetting['userId'];
+    settingType: usersSetting['type'];
+  }) {
+    return prismaConnection.usersSetting.findFirst({
+      where: { userId, type: settingType },
+    });
+  }
+
+  upsertBySettingId({
+    prismaConnection,
+    settingId,
+    userId,
+    settingType,
+    content,
+  }: {
+    prismaConnection: PrismaConnection;
+    settingId: usersSetting['id'];
+    userId: usersSetting['userId'];
+    settingType: usersSetting['type'];
+    content: usersSetting['content'];
+  }) {
+    return prismaConnection.usersSetting.upsert({
+      where: { id: settingId },
+      update: {
+        type: settingType,
+        content,
+      },
+      create: { userId, content, type: settingType },
+    });
+  }
+}
