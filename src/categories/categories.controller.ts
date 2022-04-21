ffc0de0fd9bc73052a3dfb799ab36e362e7bb68c
argon1025 <argon1025@gmail.com>
2022-04-21 19:26:21 +0900

    feat: 유저가 사용한 카테고리 조회 Controller

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,9 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Query } from '@nestjs/common';
 
-import { getCategories } from '@api/categories/categories.decorator';
+import {
+  getCategories,
+  getUserCategories,
+} from '@api/categories/categories.decorator';
 import { CategoriesService } from '@api/categories/categories.service';
 
 import { GetCategoriesResponseDto } from '@api/categories/dto/get-categories.dto';
+import {
+  GetUserCategoriesRequestQueryDto,
+  GetUserCategoriesResponseDto,
+} from '@api/categories/dto/get-user-categories.dto';
 
 @Controller('categories')
 export class CategoriesController {
@@ -13,6 +20,15 @@ export class CategoriesController {
   async getCategories() {
     return new GetCategoriesResponseDto({
       list: await this.categoriesService.getAllCategories(),
+    });
+  }
+
+  @getUserCategories()
+  async getUsersCategories(
+    @Query() { userId }: GetUserCategoriesRequestQueryDto,
+  ) {
+    return new GetUserCategoriesResponseDto({
+      list: await this.categoriesService.getUserCategories(userId),
     });
   }
 }
