15740dcfcdce67f67249b769e5f99f39bfec37a1
Seongrok.lee <55491354+argon1025@users.noreply.github.com>
2022-05-01 17:21:29 +0900

    Date 테스트코드 작성 (#66)
    * chore: 루트 폴더에서부터 테스트 코드를 탐색하도록 변경
    
    * test: DateService 테스트코드 작성
    
    * refactor: 불필요한 DateService 메서드 삭제

--- library/date/date.ts
+++ library/date/date.ts
@@ -2,7 +2,6 @@ import * as dateFns from 'date-fns';
 import { formatISO } from 'date-fns';
 
 export const now = (date?: string) => (date ? new Date(date) : new Date());
-export const dateToISO = (date: Date) => dateFns.formatISO(date);
 
 // NOTE: 오늘부터 인자로 받은일 만큼의 날자를 배열로 리턴합니다
 export const arrayOfLastDate = (date: Date, range: number) => {
