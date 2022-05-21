6ee1e3c9a89f558dde6e56fd765499d3d66013df
argon1025 <argon1025@gmail.com>
2022-05-21 16:52:38 +0900

    feat: 전체 엔드포인트 데코레이터에 Swagger 설명 추가

--- src/categories/categories.decorator.ts
+++ src/categories/categories.decorator.ts
@@ -1,5 +1,5 @@
 import { applyDecorators, Get } from '@nestjs/common';
-import { ApiOkResponse } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { GetCategoriesResponseDto } from '@api/categories/dto/get-categories.dto';
 import { GetUserCategoriesResponseDto } from '@api/categories/dto/get-user-categories.dto';
@@ -7,6 +7,10 @@ import { GetUserCategoriesResponseDto } from '@api/categories/dto/get-user-categ
 export const GetCategories = () =>
   applyDecorators(
     Get(),
+    ApiTags('Category'),
+    ApiOperation({
+      summary: '전체 카테고리 리스트를 요청합니다.',
+    }),
     ApiOkResponse({
       description: '카테고리 조회 성공',
       type: GetCategoriesResponseDto,
@@ -16,6 +20,10 @@ export const GetCategories = () =>
 export const GetUserCategories = () =>
   applyDecorators(
     Get('user'),
+    ApiTags('Category'),
+    ApiOperation({
+      summary: '유저가 포스트를 작성할 때 사용한 카테고리 리스트를 요청합니다.',
+    }),
     ApiOkResponse({
       description: '유저가 사용한 카테고리 리스트 조회 완료',
       type: GetUserCategoriesResponseDto,
--- src/comments/comments.decorator.ts
+++ src/comments/comments.decorator.ts
@@ -1,5 +1,5 @@
 import { applyDecorators, Delete, Get, Post, Put } from '@nestjs/common';
-import { ApiOkResponse } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { JwtAccessTokenGuard } from '@app/library/guards/auth/jwt-access-token.guard';
 import { GetCommentsResponseDto } from '@api/comments/dto/get-comments.dto';
@@ -8,12 +8,20 @@ export const CreateComments = () =>
   applyDecorators(
     Post(),
     JwtAccessTokenGuard(),
+    ApiTags('Comment'),
+    ApiOperation({
+      summary: '댓글을 생성합니다.',
+    }),
     ApiOkResponse({ description: '댓글 생성 성공', type: undefined }),
   );
 
 export const GetComments = () =>
   applyDecorators(
     Get(),
+    ApiTags('Comment'),
+    ApiOperation({
+      summary: '댓글 리스트를 요청합니다.',
+    }),
     ApiOkResponse({
       description: '댓글 리스트 로드 성공',
       type: GetCommentsResponseDto,
@@ -24,6 +32,10 @@ export const DeleteComment = () =>
   applyDecorators(
     Delete(),
     JwtAccessTokenGuard(),
+    ApiTags('Comment'),
+    ApiOperation({
+      summary: '본인이 작성한 댓글을 삭제합니다.',
+    }),
     ApiOkResponse({
       description: '댓글 삭제 성공',
       type: undefined,
@@ -34,6 +46,10 @@ export const UpdateComment = () =>
   applyDecorators(
     Put(),
     JwtAccessTokenGuard(),
+    ApiTags('Comment'),
+    ApiOperation({
+      summary: '본인이 작성한 댓글을 수정합니다.',
+    }),
     ApiOkResponse({
       description: '댓글 수정 성공',
       type: undefined,
