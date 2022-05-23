eec0df6310f531c00053d37d148bfc437bafba2f
argon1025 <argon1025@gmail.com>
2022-05-23 20:23:27 +0900

    feat: 포스트 수정에 필요한 Service, 예외 메시지 작성

--- src/posts/errors/posts.error.ts
+++ src/posts/errors/posts.error.ts
@@ -10,3 +10,8 @@ export const isPrivatePost: ExceptionMessageInterface = {
   [COUNTRY.ko]: '비밀글 입니다',
   [COUNTRY.en]: "It's a secret post",
 };
+
+export const notPostYouOwn: ExceptionMessageInterface = {
+  [COUNTRY.ko]: '본인이 소유한 게시글만 수정할 수 있습니다',
+  [COUNTRY.en]: 'This is not a post you own',
+};
