0235f123a74cb850de3cedd75559a2cfac53a1a7
argon1025 <argon1025@gmail.com>
2022-05-23 21:23:53 +0900

    feat: CORS 환경설정 main 파일에 반영

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from '@api/app.module';
 import { ConfigService } from '@nestjs/config';
-import * as cookieParser from 'cookie-parser';
+import { NestFactory } from '@nestjs/core';
 
+import { AppModule } from '@api/app.module';
 import { swaggerDocumentBuilder } from '@app/library/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get<ConfigService>(ConfigService);
-  const SERVER_PORT = configService.get<number>('SERVER_PORT', 3000);
+
+  const origin: string[] = configService
+    .get<string>('CORS_ORIGIN', '*')
+    .split(',');
+  const methods: string[] = configService
+    .get<string>('CORS_METHOD', 'GET,HEAD,PUT,PATCH,POST,DELETE')
+    .split(',');
+  app.enableCors({ origin, methods });
 
   app.use(cookieParser());
   swaggerDocumentBuilder(app);
 
+  const SERVER_PORT = configService.get<number>('SERVER_PORT', 3000);
   await app.listen(SERVER_PORT);
 }
 bootstrap();
