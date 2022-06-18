26cf31b4237343c0b8c5b4054eaf2f93986b0050
argon1025 <argon1025@gmail.com>
2022-06-18 23:08:12 +0900

    feat: 유저 정보를 조회하는 엔드포인트 추가

--- src/users/users.decorator.ts
+++ src/users/users.decorator.ts
@@ -4,6 +4,7 @@ import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { JwtAccessTokenGuard } from '@app/library/guards/auth/jwt-access-token.guard';
 
 import { GetMeResponseDto } from '@api/users/dto/get-me.dto';
+import { GetUserProfileResponseDto } from '@api/users/dto/get-user-profile.dto';
 
 export const SetSetting = () =>
   applyDecorators(
@@ -16,13 +17,26 @@ export const SetSetting = () =>
     ApiOkResponse({ description: '유저 설정 반영', type: undefined }),
   );
 
-export const getMe = () =>
+export const GetMe = () =>
   applyDecorators(
     Get('me'),
     JwtAccessTokenGuard(),
     ApiTags('User'),
     ApiOperation({
-      summary: '유저정보를 요청합니다.',
+      summary: '로그인된 유저정보를 요청합니다.',
     }),
     ApiOkResponse({ description: '내 정보 로드완료', type: GetMeResponseDto }),
+  );
+
+export const GetUserProfile = () =>
+  applyDecorators(
+    Get('profile/:userId'),
+    ApiTags('User'),
+    ApiOperation({
+      summary: '특정 유저정보를 요청합니다.',
+    }),
+    ApiOkResponse({
+      description: '유저 정보 로드완료',
+      type: GetUserProfileResponseDto,
+    }),
   );
